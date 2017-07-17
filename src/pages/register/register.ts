fa0511d98fb83d8e6fdfaf2e48fe594e7a047f97
Krishna Teja Dinavahi <Krishna.DINAVAHI@amadeus.com>
2017-07-17 17:59:33 -0400

    Validations for register

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -18,8 +18,9 @@ private registrationForm : FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
     this.registrationForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
       userName: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      password: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]]
     });
   }
 
@@ -33,6 +34,10 @@ private registrationForm : FormGroup;
 
   validatePassword() {
     return !this.registrationForm.controls.password.pristine && !this.registrationForm.controls.password.valid;
+  }
+
+  validateName() {
+    return !this.registrationForm.controls.name.pristine && !this.registrationForm.controls.name.valid;
   }
 
 }
