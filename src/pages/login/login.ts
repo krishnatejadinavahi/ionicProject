43ae1445f291831c6def20a11514830d9f120528
Krishna Teja Dinavahi <Krishna.DINAVAHI@amadeus.com>
2017-07-23 13:56:09 -0400

    Navigating to user portal from login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, App } from 'ionic-angular';
 import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 import {RegisterPage} from "../register/register";
 import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
 import { RegisterServiceProvider } from "../../providers/register-service/register-service";
 import { ToastController } from 'ionic-angular';
+import { UserPortalPage } from '../user-portal/user-portal';
 
 @Component({
   selector: 'page-login',
@@ -17,8 +18,11 @@ private loginForm : FormGroup;
 private registerPage = RegisterPage;
 
   constructor(public navCtrl: NavController, 
-    public navParams: NavParams, private formBuilder: FormBuilder, 
-    private http : Http, private registerService: RegisterServiceProvider, private toastCtrl: ToastController) {
+    public navParams: NavParams, 
+    private formBuilder: FormBuilder, 
+    private http : Http, 
+    private registerService: RegisterServiceProvider, 
+    private toastCtrl: ToastController, private appCtrl: App) {
     this.loginForm = this.formBuilder.group({
       userName: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]],
@@ -37,15 +41,9 @@ private registerPage = RegisterPage;
     return !this.loginForm.controls.password.pristine && !this.loginForm.controls.password.valid;
   }
 
-  /* sendRequest() {
-    this.http.get("http://localhost").map(res => res.json()).subscribe(function test(res) {
-      console.log(res);
-    });
-  } */
-
   login() {
     this.registerService.login(this.loginForm.value).then((response) => {
-      console.log(response);
+      this.appCtrl.getRootNav().push(UserPortalPage);
     }, 
     (err) => {
       this.toastCtrl.create({
